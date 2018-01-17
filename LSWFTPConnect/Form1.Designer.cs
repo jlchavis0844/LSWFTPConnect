9009b7ffcd930e5e18766cfcea87bc22511ec1a7
jchavis <jchavis@DBA>
2018-01-17 15:53:56 -0800

    Trying to roll back

--- LSWFTPConnect/Form1.Designer.cs
+++ LSWFTPConnect/Form1.Designer.cs
@@ -23,12 +23,190 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.btnFiles = new System.Windows.Forms.Button();
+            this.lbFiles = new System.Windows.Forms.ListBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnOutputDir = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.tbServer = new System.Windows.Forms.TextBox();
+            this.lblServer = new System.Windows.Forms.Label();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnStore = new System.Windows.Forms.Button();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.btnLocalFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFiles
+            // 
+            this.btnFiles.Location = new System.Drawing.Point(13, 149);
+            this.btnFiles.Name = "btnFiles";
+            this.btnFiles.Size = new System.Drawing.Size(118, 23);
+            this.btnFiles.TabIndex = 1;
+            this.btnFiles.Text = "Load Remote Files";
+            this.btnFiles.UseVisualStyleBackColor = true;
+            this.btnFiles.Click += new System.EventHandler(this.btnFiles_Click);
+            // 
+            // lbFiles
+            // 
+            this.lbFiles.FormattingEnabled = true;
+            this.lbFiles.Location = new System.Drawing.Point(13, 180);
+            this.lbFiles.Name = "lbFiles";
+            this.lbFiles.Size = new System.Drawing.Size(375, 485);
+            this.lbFiles.TabIndex = 2;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(97, 99);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(63, 13);
+            this.lblOutput.TabIndex = 3;
+            this.lblOutput.Text = "H:\\Desktop";
+            // 
+            // btnOutputDir
+            // 
+            this.btnOutputDir.Location = new System.Drawing.Point(13, 94);
+            this.btnOutputDir.Name = "btnOutputDir";
+            this.btnOutputDir.Size = new System.Drawing.Size(78, 23);
+            this.btnOutputDir.TabIndex = 4;
+            this.btnOutputDir.Text = "Output Dir";
+            this.btnOutputDir.UseVisualStyleBackColor = true;
+            this.btnOutputDir.Click += new System.EventHandler(this.btnOutputDir_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(137, 150);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(118, 23);
+            this.btnGo.TabIndex = 5;
+            this.btnGo.Text = "Process Selected File";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 676);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(16, 13);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "...";
+            // 
+            // tbServer
+            // 
+            this.tbServer.Location = new System.Drawing.Point(97, 12);
+            this.tbServer.Name = "tbServer";
+            this.tbServer.Size = new System.Drawing.Size(291, 20);
+            this.tbServer.TabIndex = 7;
+            this.tbServer.Text = "FTPS.NATIONALLIFE.COM";
+            // 
+            // lblServer
+            // 
+            this.lblServer.AutoSize = true;
+            this.lblServer.Location = new System.Drawing.Point(12, 15);
+            this.lblServer.Name = "lblServer";
+            this.lblServer.Size = new System.Drawing.Size(79, 13);
+            this.lblServer.TabIndex = 8;
+            this.lblServer.Text = "Server Address";
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(12, 41);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(60, 13);
+            this.lblUserName.TabIndex = 10;
+            this.lblUserName.Text = "User Name";
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Location = new System.Drawing.Point(97, 38);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(291, 20);
+            this.tbUserName.TabIndex = 9;
+            this.tbUserName.Text = "RALotterAgencyBuilders";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(12, 67);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 12;
+            this.lblPassword.Text = "Password";
+            // 
+            // btnStore
+            // 
+            this.btnStore.Location = new System.Drawing.Point(346, 62);
+            this.btnStore.Name = "btnStore";
+            this.btnStore.Size = new System.Drawing.Size(42, 23);
+            this.btnStore.TabIndex = 13;
+            this.btnStore.Text = "Store";
+            this.btnStore.UseVisualStyleBackColor = true;
+            this.btnStore.Click += new System.EventHandler(this.btnStore_Click);
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(97, 64);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(243, 20);
+            this.tbPassword.TabIndex = 11;
+            // 
+            // btnLocalFile
+            // 
+            this.btnLocalFile.Location = new System.Drawing.Point(13, 121);
+            this.btnLocalFile.Name = "btnLocalFile";
+            this.btnLocalFile.Size = new System.Drawing.Size(375, 23);
+            this.btnLocalFile.TabIndex = 14;
+            this.btnLocalFile.Text = "Load and Process Local File";
+            this.btnLocalFile.UseVisualStyleBackColor = true;
+            this.btnLocalFile.Click += new System.EventHandler(this.btnLocalFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 696);
+            this.Controls.Add(this.btnLocalFile);
+            this.Controls.Add(this.btnStore);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.tbUserName);
+            this.Controls.Add(this.lblServer);
+            this.Controls.Add(this.tbServer);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnOutputDir);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lbFiles);
+            this.Controls.Add(this.btnFiles);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button btnFiles;
+        private System.Windows.Forms.ListBox lbFiles;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnOutputDir;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox tbServer;
+        private System.Windows.Forms.Label lblServer;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnStore;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button btnLocalFile;
     }
 }
 
--- LSWFTPConnect/Form1.cs
+++ LSWFTPConnect/Form1.cs
@@ -1,17 +1,227 @@
-﻿using System;
+﻿using FluentFTP;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Security.Authentication;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace LSWFTPConnect {
     public partial class Form1 : Form {
+        static List<FtpListItem> files = new List<FtpListItem>(); //file list
+        static string folderName = ""; 
+        static FtpClient client;
+        List<string> lifeLines;
+        List<string> annLines;
+        static PasswordRepository passRep = new PasswordRepository();
+
         public Form1() {
             InitializeComponent();
+
+            if(passRep.GetPassword() != null && passRep.GetPassword() != "") {
+                tbPassword.Text = passRep.GetPassword();
+            }
+
+            if(passRep.GetPassword() == null || passRep.GetPassword() == "") {
+                btnFiles.Enabled = false;
+                btnGo.Enabled = false;
+            }
+        }
+
+        private void btnFiles_Click(object sender, EventArgs e) {
+            try {
+                lblStatus.Text = "Connecting...";
+                ConnectFTP();
+                lblStatus.Text = "Connected! Getting listings...";
+                lblStatus.Refresh();
+                foreach (FtpListItem item in client.GetListing("/")) {
+                    Console.WriteLine(item);
+                    if (item.Type == FtpFileSystemObjectType.File && item.FullName.Contains(@"/AGY")) {
+                        files.Add(item);
+                    }
+                }
+
+                files.Sort(delegate (FtpListItem lhs, FtpListItem rhs) {
+                    if (lhs.Modified == null && rhs.Modified == null) return 0;
+                    else if (lhs.Modified == null) return -1;
+                    else if (rhs.Modified == null) return 1;
+                    else return lhs.Modified.CompareTo(rhs.Modified)*-1;
+                });
+
+                foreach (FtpListItem item in files) {
+                    lbFiles.Items.Add(item.FullName.ToString());
+                }
+
+                lblStatus.Text = "Files Loaded, please choose one to process";
+            } catch (Exception ex) {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+
+        void OnValidateCertificate(FtpClient control, FtpSslValidationEventArgs e) {
+            // add logic to test if certificate is valid here
+            e.Accept = true;
+        }
+
+        private void btnOutputDir_Click(object sender, EventArgs e) {
+            // Show the FolderBrowserDialog.
+            FolderBrowserDialog fbd = new FolderBrowserDialog() ;
+            DialogResult result = fbd.ShowDialog();
+            if (result == DialogResult.OK) {
+                folderName = fbd.SelectedPath;
+                lblOutput.Text = folderName;
+            }
+        }
+
+        private void btnGo_Click(object sender, EventArgs e) {
+            if(lbFiles.SelectedIndex == -1) {
+                lblStatus.Text = "No File Selected";
+                return;
+            }
+
+            if (!client.IsConnected) {
+                ConnectFTP();
+                lblStatus.Text = "Reconnecting..";
+            }
+            string selectedFile = lbFiles.Items[lbFiles.SelectedIndex].ToString();
+            string fileName = Path.GetFileName(selectedFile);
+            string outFile = lblOutput.Text + "\\" + fileName;
+
+            string commPath = @"P:\RALFG\Common Files\Commissions & Insurance\Commission Statements\" +
+                DateTime.Now.Year.ToString() + @"\LSW Combo\";
+            lblStatus.Text = "Downloading " + fileName;
+            client.DownloadFile(commPath + fileName, selectedFile);
+            client.Disconnect();
+            lblStatus.Text = "Done downloading file. Processesing";
+            ProcessCommReport(commPath + fileName, lblOutput.Text + "\\" +
+                Path.GetFileNameWithoutExtension(selectedFile));
+            lblStatus.Text = "Done Processesing";
+        }
+
+        private void ConnectFTP() {
+            try {
+                client = new FtpClient();
+                client.Host = tbServer.Text;
+                Console.WriteLine(passRep.GetPassword());
+                client.Credentials = new NetworkCredential(tbUserName.Text, passRep.GetPassword());
+                client.EncryptionMode = FtpEncryptionMode.Explicit;
+                client.SslProtocols = SslProtocols.Tls | SslProtocols.Tls11 | SslProtocols.Tls12;
+                client.ValidateCertificate += new FtpSslValidation(OnValidateCertificate);
+                client.Connect();
+                lblStatus.Text = "Connected";
+            } catch(Exception ex) {
+                lblStatus.Text = ex.Message.ToString();
+            }
+        }
+
+        protected virtual bool IsFileLocked(FileInfo file) {
+            FileStream stream = null;
+
+            try {
+                stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            }
+            catch (IOException) {
+                return true;
+            }
+            finally {
+                if (stream != null)
+                    stream.Close();
+            }
+            return false;
+        }
+
+        private void ProcessCommReport(string inFile, string outFile) {
+            XDocument xdoc = XDocument.Load(inFile);
+            XNamespace ns0 = xdoc.Root.GetNamespaceOfPrefix("ns0");
+            var oLifes = from data in xdoc.Descendants()
+                         where data.Name.LocalName == "OLifE"
+                         select data;
+
+            lifeLines = new List<string>();
+            annLines = new List<string>();
+
+            foreach (var data in oLifes) {//parent of the commission data is OLifE (1 to 1)
+                var plan = data.Element(ns0 + "Holding").Element(ns0 + "Policy").Element(ns0 + "PlanName").Value;
+                var date = data.Element(ns0 + "Holding").Element(ns0 + "Policy").Element(ns0 + "IssueDate").Value;
+                var sDate = data.Element(ns0 + "FinancialStatement").Element(ns0 + "StatementDate").Value;
+                var commDet = data.Element(ns0 + "FinancialStatement").Element(ns0 + "CommissionStatement")
+                    .Element(ns0 + "CommissionDetail");
+
+                var amt = commDet.Element(ns0 + "EarnedAmt").Value;
+                var polNum = commDet.Attribute("HoldingID").Value;
+                var owner = commDet.Attribute("OwnerPartyID").Value;
+                var commRate = commDet.Element(ns0 + "CommissionRate").Value;
+                var split = commDet.Element(ns0 + "SplitPercent").Value;
+                var prem = commDet.Element(ns0 + "PaymentBasisAmt").Value;
+                var type = commDet.Element(ns0 + "CommissionType").Value;
+
+                string line = polNum + ", " + owner + ", " + plan + ", " + date + ", " +
+                    prem + ", " + commRate + ", " + split + ", ";
+
+                if (type == "Renewal") {
+                    line += ("0, " + amt.ToString());
+                } else {
+                    line += amt.ToString() + ", 0";
+                }
+
+                //line += ", " + sDate;
+
+               // Console.WriteLine(line);
+                if (polNum.StartsWith("LS")) {
+                    lifeLines.Add(line);
+                } else annLines.Add(line);
+            }
+
+            StreamWriter writer;
+            if (annLines.Count > 0) {//write annuity lines
+                writer = new StreamWriter(outFile + "_AnnOut.csv");
+                writer.WriteLine("Policy, Full Name, Plan, Issue Date, Premium, Rate %, Rate, Commission, Renewal");
+                foreach (string item in annLines) {
+                    writer.WriteLine(item);
+                }
+                writer.Close();
+            }
+
+            if (lifeLines.Count > 0) {//write life lines
+                writer = new StreamWriter(outFile + "_LifeOut.csv");
+                writer.WriteLine("Policy, Full Name, Plan, Issue Date, Premium, Rate %, Rate, Commission, Renewal");
+                foreach (string item in lifeLines) {
+                    writer.WriteLine(item);
+                }
+                writer.Close();
+            }
+
+            System.Diagnostics.Process.Start(outFile + "_LifeOut.csv");
+            System.Diagnostics.Process.Start(outFile + "_AnnOut.csv");
+        }
+
+        private void btnStore_Click(object sender, EventArgs e) {
+            passRep.SavePassword(tbPassword.Text);
+            btnGo.Enabled = true;
+            btnFiles.Enabled = true;
+        }
+
+        private void btnLocalFile_Click(object sender, EventArgs e) {
+            // Show the FolderBrowserDialog.
+            OpenFileDialog ofd = new OpenFileDialog();
+            string commPath = @"P:\RALFG\Common Files\Commissions & Insurance\Commission Statements\" +
+                DateTime.Now.Year.ToString() + @"\LSW Combo\";
+            ofd.InitialDirectory = commPath;
+            ofd.Filter = "XML Files|*.xml";
+            DialogResult result = ofd.ShowDialog();
+            
+            if (result == DialogResult.OK) {
+                string path = ofd.FileName;
+                string outFileLocal = lblOutput.Text + "\\" + Path.GetFileNameWithoutExtension(path);
+                //lblOutput.Text = path;
+                ProcessCommReport(path, outFileLocal);
+                lblStatus.Text = "Done Processesing";
+
+            }
         }
     }
 }
